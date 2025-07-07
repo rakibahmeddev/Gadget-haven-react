0ae63ade553e6e3cff42c0bed6a048b7dffa6979
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-07-07 20:03:31 +0600

    folder structure made under components Navbar and Footer is created from preline and under Layout root.jsx is located and it has header, outlet and footer, then under pages all of the pages will be available like home, and into the main.jsx I created routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './index.css';
 import 'preline/preline';
+import Root from './layout/root';
+import Home from './Pages/Home';
 
 // You can run this inside useEffect or after DOM loaded
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,8 +15,21 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  
+}]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
