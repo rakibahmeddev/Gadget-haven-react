e50aa08daf8c1a4e4f9e788d0aa964a03546670f
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-07-10 16:00:37 +0600

    single product page done

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
+import { Link, useLoaderData, useParams } from 'react-router-dom';
+import { RiStarSLine } from 'react-icons/ri';
+import { MdOutlineShoppingCart } from 'react-icons/md';
+import { IoMdHeartEmpty } from 'react-icons/io';
 
 const ProductDetail = () => {
   const { productId } = useParams();
@@ -20,7 +23,7 @@ const ProductDetail = () => {
   } = newProduct;
 
   return (
-    <div className='bg-gray-100'>
+    <div className="bg-gray-100 pb-26">
       {/* Hero */}
       <div className="bg-purple-600">
         <div className=" px-4 sm:px-6 lg:px-8 pt-10 pb-48">
@@ -38,123 +41,82 @@ const ProductDetail = () => {
 
           <div className="max-w-3xl text-center mx-auto">
             <p className="text-lg text-white/70">
-            Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!
+              Explore the latest gadgets that will take your experience to the
+              next level. From smart devices to the coolest accessories, we have
+              it all!
             </p>
           </div>
-
         </div>
       </div>
       {/* End Hero */}
 
       {/* Features */}
-      <div className="max-w-[85rem] bg-white rounded-4xl p-6 md:p-8 lg:p-10 mx-auto mt-[-150px]">
-        {/* Grid */}
-        <div className="flex gap-x-10">
-          <div>
-            <img
-              className="rounded-xl"
-              src={image}
-              alt={title}
-            />
-          </div>
-          {/* End Col */}
-
-          <div className="mt-5 sm:mt-10 lg:mt-0">
-            <div className="">
-              {/* Title */}
-              <div className="">
-                <h2 className="font-bold text-3xl lg:text-4xl text-gray-800">
-                  We tackle the challenges start-ups face
-                </h2>
-                <p className="text-gray-500">
-                  Besides working with start-up enterprises as a partner for
-                  digitalization, we have built enterprise products for common
-                  pain points that we have encountered in various products and
-                  projects.
-                </p>
-              </div>
-              {/* End Title */}
-
-              {/* List */}
-              <ul className="space-y-2 sm:space-y-4">
-                <li className="flex gap-x-3">
-                  <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600">
-                    <svg
-                      className="shrink-0 size-3.5"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <polyline points="20 6 9 17 4 12" />
-                    </svg>
-                  </span>
-                  <div className="grow">
-                    <span className="text-sm sm:text-base text-gray-500">
-                      <span className="font-bold">Easy & fast</span> designing
-                    </span>
-                  </div>
-                </li>
-
-                <li className="flex gap-x-3">
-                  <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600">
-                    <svg
-                      className="shrink-0 size-3.5"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <polyline points="20 6 9 17 4 12" />
-                    </svg>
-                  </span>
-                  <div className="grow">
-                    <span className="text-sm sm:text-base text-gray-500">
-                      Powerful <span className="font-bold">features</span>
-                    </span>
-                  </div>
-                </li>
-
-                <li className="flex gap-x-3">
-                  <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600">
-                    <svg
-                      className="shrink-0 size-3.5"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <polyline points="20 6 9 17 4 12" />
-                    </svg>
-                  </span>
-                  <div className="grow">
-                    <span className="text-sm sm:text-base text-gray-500">
-                      User Experience Design
-                    </span>
-                  </div>
-                </li>
-              </ul>
-              {/* End List */}
+      <div className=" px-22 lg:px-0">
+        <div className=' md:max-w-[85rem] bg-white rounded-4xl p-6 md:p-8 lg:p-10 mx-auto mt-[-150px]'>
+          {/* Grid */}
+          <div className="md:flex gap-x-10">
+            <div className="flex justify-center items-center border border-gray-200 rounded-2xl p-10 shadow-2xl">
+              <img
+                className="rounded-2xl w-[50%] md:w-[600px] block"
+                src={image}
+                alt={title}
+              />
             </div>
+            {/* End Col */}
+
+            <div className="mt-5 sm:mt-10 lg:mt-0">
+              <div className="">
+                {/* Title */}
+                <div className="">
+                  <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl text-gray-800">
+                    {title}
+                  </h2>
+                  <p className="text-black font-bold">
+                    Price:{' '}
+                    <span className="font-bold text-2xl text-purple-600">
+                      ${price}
+                    </span>
+                  </p>
+
+                  <p className="text-black font-normal my-3">{description}</p>
+                </div>
+                {/* End Title */}
+
+                <div className="">
+                  <div>
+                    <button className="bg-[#EAF5E6] text-green-500 px-6 py-1 font-light text-sm rounded-full">
+                      {category}
+                    </button>
+                  </div>
+                  <div className="flex gap-1 justify-start items-center mt-10">
+                    <p>Ratting: </p>
+                    <span className=" ml-3">{rating.rate}</span>
+                    <RiStarSLine className="text-yellow-500 text-2xl" />
+                    <span>/ {rating.count}</span>
+                  </div>
+
+                  <div className="mt-8 flex items-center">
+                    <button></button>
+                    <Link
+                      to=""
+                      className="btn px-12 py-3 flex justify-center items-center gap-x-1 bg-purple-600 text-white rounded-lg"
+                    >
+                      {' '}
+                      <p>Add to Cart</p>{' '}
+                      <MdOutlineShoppingCart className="text-xl" />
+                    </Link>
+
+                    <Link to="" className="bg-green-500 p-3 rounded-full ml-8">
+                      <IoMdHeartEmpty className="text-2xl text-white" />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* End Col */}
           </div>
-          {/* End Col */}
+          {/* End Grid */}
         </div>
-        {/* End Grid */}
       </div>
       {/* End Features */}
     </div>
